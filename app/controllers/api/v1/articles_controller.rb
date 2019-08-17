ba1a7b5cba5845448acf32eb1830742354ced56c
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-17 15:32:43 -0500

    add the destroy and update methods to the controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -21,6 +21,20 @@ module Api
         end
       end
 
+      def destroy
+        article = Article.destroy(params[:id])
+        render json: {status: 'SUCCESS', message: 'Deleted the Article', data: article}, status: 200
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: {status: 'SUCCESS', message: 'Article Updated', data: article}, status: 200
+        else
+          render json: {status: 'ERROR', message: 'Article Not Updated', data: article.errors}, status: :unprocessable_entity
+        end
+      end
+
       private
       def article_params
         params.permit(:title, :body)
