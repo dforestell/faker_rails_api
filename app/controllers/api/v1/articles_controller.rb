95af539b098380c39fec57736028fed635a36f6d
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-17 15:07:50 -0500

    fix a syntax error in the articles controller and add working index and show methods for the article resource

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,12 +1,16 @@
 module Api
-  module v1
+  module V1
     class ArticlesController < ApplicationController
 
       def index
         articles = Article.order('created_at DESC')
-        render json: {status: 'SUCCESS', message: 'Loaded Articles', data: articles}, status: ok
+        render json: {status: 'SUCCESS', message: 'Loaded Articles', data: articles}, status: 200
       end
 
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded Article', data: article}, status: 200
+      end
 
     end
   end
