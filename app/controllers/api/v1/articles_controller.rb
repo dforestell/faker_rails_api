d819bef16b47b71c080a46af3c34c6d5dc48c923
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-17 14:50:46 -0500

    add api folder and v1 folder then create the articles controller and routes

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module v1
+    class ArticlesController < ApplicationController
+
+      def index
+        articles = Article.order('created_at DESC')
+        render json: {status: 'SUCCESS', message: 'Loaded Articles', data: articles}, status: ok
+      end
+
+
+    end
+  end
+end
